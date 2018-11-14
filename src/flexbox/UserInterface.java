d1f8f2f5db5ef9874f1f4e2e38b60f315986e314
WillGreen98 <will.green98@hotmail.com>
2018-11-14 13:41:34 +0000

    Window Size Refactor

--- src/flexbox/UserInterface.java
+++ src/flexbox/UserInterface.java
@@ -4,11 +4,14 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 
 class UserInterface {
+    private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 600;
+
     private JFrame frame = new JFrame("ADPROC - FlexBox CW");
     
     UserInterface() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600,400);
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         frame.setVisible(true);
     }
 }
