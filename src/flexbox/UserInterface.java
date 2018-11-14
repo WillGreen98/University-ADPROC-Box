ba7eefa4a0584acdca5cf3ed91c8a74757967798
WillGreen98 <will.green98@hotmail.com>
2018-11-14 13:21:07 +0000

    GUI Updates

--- src/flexbox/UserInterface.java
+++ src/flexbox/UserInterface.java
@@ -1,11 +1,12 @@
 package flexbox;
 
-/**
- *
- * @author Matthew Hopson
- */
-public class UserInterface {
-    
-   
-    
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+class UserInterface {
+    public static void main(String args[]){
+        JFrame frame = new JFrame("ADPROC - FlexBox CW");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,400);
+    }
 }
