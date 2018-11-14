8fca0eb7a3e5ef9bebd48b2346c456484b0cae5c
Hopson97 <mhopson@hotmail.co.uk>
2018-11-14 13:43:09 +0000

    mergeeeeeeeeeeeeeee

--- src/flexbox/UserInterface.java
+++ src/flexbox/UserInterface.java
@@ -2,6 +2,8 @@ package flexbox;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 class UserInterface {
     private JFrame frame = new JFrame("ADPROC - FlexBox CW");
@@ -10,5 +12,13 @@ class UserInterface {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600,400);
         frame.setVisible(true);
+        
+                JTextField field = new JTextField("Enter");
+        frame.add(field);
+        JPanel p = new JPanel();
+        p.add(field);
+        frame.add(p);
+        
+
     }
 }
