f0fb1c373147e114b6a7b62551d2ddc9aedc6ca6
Hopson97 <mhopson@hotmail.co.uk>
2018-11-18 17:08:48 +0000

    Add docs to basket item info class

--- src/flexbox/ui/BasketItemInfo.java
+++ src/flexbox/ui/BasketItemInfo.java
@@ -3,22 +3,35 @@ package flexbox.ui;
 import flexbox.boxtypes.Box;
 
 /**
- *
- * @author Matthew Hopson
+ * Contains information about basket items
+ * @author Group D4
  */
 public class BasketItemInfo {
     private Box box;
     private int boxType;
     
+    /**
+     * Constructs BasketItemInfo
+     * @param box The box that is being added to the basket
+     * @param boxType The box type of the box
+     */
     public BasketItemInfo (Box box, int boxType) {
         this.box = box;
         this.boxType = boxType;
     }
     
+    /**
+     * Gets the box
+     * @return The box
+     */
     public Box getBox() {
         return box;
     }
     
+    /**
+     * Gets the box type of the box in this basket item
+     * @return The type of box
+     */
     public int getBoxType() {
         return boxType;
     }
